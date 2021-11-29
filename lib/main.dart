786d477dc62268d74864327bb8757f4e5023bf35
Nerb000 <you@example.com>
2021-11-30 02:27:13 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:nerb_app/screens/home.dart';
+import 'package:nerb_app/screens/Welcome.dart';
+
+void main() => runApp(MaterialApp(
+   debugShowCheckedModeBanner: false,
+  home: Welcome(),
+      
+));
+
+
+
